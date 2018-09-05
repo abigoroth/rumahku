a6e4edabb51202f97ecd353d548d96266da35f3f
alyasuhaimi967 <alyasuhaimi96@gmail.com>
2018-09-05 16:24:48 +0800

    Admin: login, register resident Resident : login guard:login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,35 @@
 Rails.application.routes.draw do
+  
+  resources :infos
+  resources :res
+  resources :guests
+  get 'pages/guestregister'
+  get 'pages/user'
+  get 'pages/guard'
+  get 'pages/admin'
+  devise_for :guards
+  devise_for :admins
+  devise_for :users
+  root to: redirect('/pages/main')
+  get 'pages/main'
+ 
+
+  resources :residents
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+namespace :admin do
+  resources :posts
+  root to: redirect('/pages/admin')
 end
+
+namespace :user do
+  resources :posts
+  root to: redirect('/pages/user')
+end
+
+namespace :guard do
+  resources :posts
+  root to: redirect('/pages/guard')
+end
+
+end
+
