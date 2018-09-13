bb6a83037488c286601b18b8d8c2176cec3a3529
alyasuhaimi967 <alyasuhaimi96@gmail.com>
2018-09-13 18:16:55 +0800

    alya

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -82,7 +82,7 @@ class ApartmentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def apartment_params
-      params.require(:apartment).permit(:Age, :ParkingNumber, :HpNumber, :NumberApartment, :Level, :ApartmentType, :parking_queue, :info_id)
+      params.require(:apartment).permit(:Age, :ParkingNumber, :HpNumber, :NumberApartment, :Level, :ApartmentType, :parking_queue, :info_id, :user_id)
     end
 
 
