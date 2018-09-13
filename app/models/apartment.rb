c40d20a089e1f34d842393183387e9ca275e5836
quzailyshariff <quzaily@gmail.com>
2018-09-13 15:52:35 +0800

    huhuhuhuu

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,7 +1,6 @@
 class Apartment < ApplicationRecord
     has_many :park_spacerentals
     has_many :cars
-    belongs_to :resident, optional: true
     belongs_to :info
-    belongs_to :resident
+   
 end
