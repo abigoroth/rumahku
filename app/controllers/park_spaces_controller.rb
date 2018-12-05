d0310eb08d1af235b9c97e71a3cdcd82fe614b80
quzailyshariff <quzaily@gmail.com>
2018-12-05 12:11:00 +0800

    hello

--- app/controllers/park_spaces_controller.rb
+++ app/controllers/park_spaces_controller.rb
@@ -88,7 +88,7 @@ class ParkSpacesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def park_space_params
-      params.require(:park_space).permit(:parknum, :spacetype, :available, :info_id, :user_id, apartments_attributes: [:id, :requested_park_space_id, :requested_start_rent, :requested_end_rent, :_destroy])
+      params.require(:park_space).permit(:parknum, :spacetype, :available, :info_id, :user_id, apartments_attributes: [:id, :requested_park_space_id, :requested_start_rent, :requested_end_rent, :_destroy],)
     end
 
 end
